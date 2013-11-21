c4ce433c8ba8e3e6e69d88f7a5c010615b5267a2
Sara Gorecki <sgorecki@gmail.com>
2013-11-21 18:39:49 -0500

    write tests for student class

---
+++ spec/student_spec.rb
@@ -0,0 +1,19 @@
+require ("../lib/student")
+
+describe Student, "#name" do
+	it "should return the name of the student" do
+		expect(Student.new("Mary", "@mary","http://mary.blog.com" ).name).to eq("Mary")
+	end
+end
+
+describe Student, "#twitter" do
+	it "should return the twitter handle of the student" do
+		expect(Student.new("Joe", "@coffeejoe", "http://coffeerox.com").twitter).to eq("@coffeejoe")
+	end
+end
+
+describe Student, "#blog" do
+	it "should return the blog url of the student" do
+		expect(Student.new("Lana", "@lalalady", "http://lala.wordpress.com").blog).to eq("http://lala.wordpress.com")
+	end
+end
